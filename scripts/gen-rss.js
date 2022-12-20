45ec42fb67b14814499188bcf3871cf1113023b7
mamvav <loopperz19@hotmail.com>
2022-12-20 23:15:05 +0700

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
+    title: 'เครดิตฟรีอัพเดต',
     site_url: 'https://blog-creditsfree.vercel.app/',
-    feed_url: 'https://yoursite.com/feed.xml'
+    feed_url: 'https://blog-creditsfree.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
